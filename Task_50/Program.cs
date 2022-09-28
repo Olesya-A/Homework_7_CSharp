58e083552fb8c3d29be69fb07c5b517343623be7
Olesya <lesusha1@yandex.ru>
2022-09-28 21:44:21 +0300

    add Task_50

---
+++ Task_50/Program.cs
@@ -0,0 +1,49 @@
+﻿// 50: Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+int height = EnterInt("Enter height: ");
+int width = EnterInt("Enter width: ");
+
+int[,] numbers = new int[height, width];
+Fill2DArray(numbers, height, width);
+Print2DArray(numbers, height, width);
+int i = EnterInt("Enter i: ");
+int j = EnterInt("Enter j: ");
+
+FindElement(numbers, height, width, i, j);
+
+int EnterInt(string prompt)
+{
+    Console.Write(prompt);
+    return int.Parse(Console.ReadLine()!);
+}
+
+void Fill2DArray(int[,] numbers, int height, int width)
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            numbers[i, j] = new Random().Next(-10, 11);
+        }
+    }
+}
+
+void Print2DArray(int[,] numbers, int height, int width)
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            Console.Write($"{numbers[i, j],3} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindElement(int[,] numbers, int height, int width, int i, int j)
+{
+    if (i >= height || j >= width)
+        Console.WriteLine("Такого элемента в массиве нет");
+    else Console.WriteLine(numbers[i, j]);
+}
